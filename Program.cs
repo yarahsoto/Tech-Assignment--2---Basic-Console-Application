64c55e2082c57176c5de09eb4a111b94896bd137
yarahsoto <yarahsoto@usf.edu>
2022-01-26 20:43:41 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tech_Assignment_2___Basic_Console_Application //Yarah Soto
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            // This is my unique comment that includes my name
+            Console.WriteLine("Hello, my name is Yarah Soto and I am currently at starbucks.");
+        }
+    }
+}
